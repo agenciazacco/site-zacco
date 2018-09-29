28d7d1c3653a220c37b314619a57ac0c27bd7b9e
Leno Lima <lenno.lima.ferreira@gmail.com>
2018-09-29 20:32:09 -0300

    alterações

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { HashLink as Link } from 'react-router-hash-link';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
 import backgroundServicos from '../../assets/imgs/servicos/background-full.jpg';
